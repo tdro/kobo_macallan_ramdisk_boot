ef024a6c853238e591be5354bbd98afc2fdc1885
tdro <tdro@github.com>
2016-08-06 15:42:36 -0400

    boot config one shot service
    lift system fps limit
    set maximum volume at runtime
    adb default persist disable
    hw disable overlays enabled as workaround

---
+++ init.macallan.boot.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+
+# set persistent FPS limits
+setprop persist.sys.NV_FPSLIMIT 1000;
+setprop persist.tegra.NV_FPSLIMIT 1000;
+
+# set alsa volume maximum
+alsa_amixer sset HP 31;
+alsa_amixer sset Speaker 31;
+
+# persist adb disabled off
+setprop persist.service.adb.enable 0;
+
+# disable hardware overlays
+while :
+do
+    sf=$(service list | grep -c "SurfaceFlinger")
+    if [ $sf -eq 1 ]
+    then
+        service call SurfaceFlinger 1008 i32 1
+        break
+    else
+        sleep 2
+    fi
+done
